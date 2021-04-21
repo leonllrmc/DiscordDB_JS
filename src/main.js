f5c7e6099c86834e246795744b994d047f06b0c8
leonllrmc <leonllrcraft@gmail.com>
2021-04-21 18:57:17 +0200

    Create main.js

---
+++ src/main.js
@@ -0,0 +1,44 @@
+class DB {
+    constructor(guild, channelId) {
+        try {
+            this.channel = guild.channels.cache.get(channelId);
+            if (!this.channel) throw new Error("Error While getting channel...");
+        }catch(e) {
+            throw e;
+        }
+    }
+    getContent() {
+        return new Promise(((resolve, reject) => {
+            this.channel.messages.fetch().then((msgs) => {
+                try {
+                    resolve(JSON.parse(msgs.first().content.toString()));
+                }catch(e) {
+                    resolve(msgs.first().content.toString());
+                }
+            }).catch((err) => {
+                reject(err);
+            })
+        }))
+    }
+    setContent(newContent) {
+        return new Promise(((resolve, reject) => {
+            if(Array.isArray(newContent)) throw new Error('Invalid content (must be JSON string or object not Array)');
+            var newContentParsed;
+            if(typeof newContent == 'string') {
+                newContentParsed = newContent;
+            }else if(typeof newContent == 'object') {
+                newContentParsed = JSON.stringify(newContent);
+            }else {
+                throw new Error(`Invalid content (must be JSON string or object not ${typeof newContent})`);
+            }
+            this.channel.messages.fetch().then((msgs) => {
+                msgs.first().edit(newContentParsed);
+                resolve();
+            }).catch((err) => {
+                reject(err);
+            })
+        }))
+    }
+}
+
+module.exports = DB;
