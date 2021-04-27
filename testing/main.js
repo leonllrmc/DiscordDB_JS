19d905aab4969ea69ca84beaa67e5f0d5c521769
leonllrmc <leonllrcraft@gmail.com>
2021-04-27 19:15:55 +0200

    Update main.js

--- testing/main.js
+++ testing/main.js
@@ -1,1 +1,13 @@
 
+const Discord = require('discord.js')
+const Discord_DB = require('../src/main.js')
+const client = new Discord.client()
+
+const DB = new Discord_DB('', '')
+(async () => {
+   const content = await DB.getContent() 
+   console.log(content)
+   await DB.setContent("{}")
+})
+
+client.login() 
